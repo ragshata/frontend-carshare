24b348aa040041d53c08f3cbbe392d224800688b
Zick Norizkue <plazzedd@outlook.com>
2025-07-24 19:56:01 +0300

    new admin

--- src/api/trial.ts
+++ src/api/trial.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8000";
+
 export async function startDriverTrial(user_id: number) {
   const res = await axios.post(`${API_BASE}/start_driver_trial`, { user_id });
   return res.data;
