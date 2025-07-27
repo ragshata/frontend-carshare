8cd70d25de6a70e64ec0bdfe92a0624af0c6e3be
Zick Norizkue <plazzedd@outlook.com>
2025-07-27 03:07:22 +0300

    new admin

--- src/pages/MyBookings.vue
+++ src/pages/MyBookings.vue
@@ -3,7 +3,7 @@
     <div class="background-img"></div>
 
     <div class="content-card">
-      <h2 class="title">Мои бронирования (debug)</h2>
+      <h2 class="title">Мои  (debug)</h2>
 
       <div class="tabs">
         <button :class="{ active: currentTab === 'active' }" @click="currentTab = 'active'">Активные</button>
