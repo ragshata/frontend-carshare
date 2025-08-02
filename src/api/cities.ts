d26c6fff84051f9e42b3246df84b52804b222efe
Zick Norizkue <plazzedd@outlook.com>
2025-08-02 17:33:30 +0300

    22

--- src/api/cities.ts
+++ src/api/cities.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export async function getCities() {
-  const { data } = await axios.get(`${API_BASE}/cities`);
+  const { data } = await axios.get(`${API_BASE}/cities/`);
   return data;
 }
